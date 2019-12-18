79a359f726843e8113f13e112b56e000b01d057d
Nguyễn Đức Huy <duchuy1169@gmail.com>
2019-12-18 22:13:45 +0700

    Add project files.

---
+++ RDP_Server/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace RDP_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPort = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPort
+            // 
+            this.txtPort.AutoSize = true;
+            this.txtPort.Location = new System.Drawing.Point(66, 41);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(29, 13);
+            this.txtPort.TabIndex = 0;
+            this.txtPort.Text = "Port:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Listen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 199);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtPort;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RDP_Server/Form2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace RDP_Server
+{
+    public partial class Form2 : Form
+    {
+        private readonly int port;
+        private TcpClient client;
+        private TcpListener server;
+        private NetworkStream ns;
+
+        private readonly Thread Listening;
+        private readonly Thread GetImage;
+        public Form2(int Port)
+        {
+            port = Port;
+            client = new TcpClient();
+            Listening = new Thread(StartListening);
+            GetImage = new Thread(ReceiveImage);
+            InitializeComponent();
+        }
+        private void StartListening()
+        {
+            while (!client.Connected)
+            {
+                server.Start();
+                client = server.AcceptTcpClient();
+            }
+            GetImage.Start();
+        }
+        private void StopListening()
+        {
+            server.Stop();
+            client = null;
+            if (Listening.IsAlive) Listening.Abort();
+            if (GetImage.IsAlive) GetImage.Abort();
+
+        }
+
+        private void ReceiveImage()
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            while (client.Connected)
+            {
+                ns = client.GetStream();
+                pictureBox1.Image = (Image)binaryFormatter.Deserialize(ns);
+            }
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            server = new TcpListener(IPAddress.Any,port);
+            Listening.Start();
+        }
+        protected override void OnFontChanged(EventArgs e)
+        {
+            base.OnFontChanged(e);
+            StopListening();
+        }
+    }
+}
